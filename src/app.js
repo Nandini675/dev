e9d3eb16e00003e6d417639224a336da3422a403
Nandini675 <nandinisharma.5698@gmail.com>
2025-03-25 00:23:54 +0530

    error handling using try catch

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require ("express");
 //craeting an instance off express js application
  const app= express();
- const {adminAuth} = require("./middleware/auth");
-// handle auth  middleware for all req GET,POST..
-app.use("/admin",adminAuth);
+ app.get("/getuserdata",(req,res)=>{
+//   logic of db call and get user data
+try{
+throw new Error("nononokn");
+   res.send("user data is sent");
+}
+catch(err){
+   res.status(404).send("user data is not");
+}
+ })
+ app.use("/",(err,req,res,next)=>{
+   if(err){
+      res.status(500).send("smthg is wrong");
+   }
+ })
+
  
-app.get("/admin/getalldata",(req,res)=>{
-   //logic of cheking if the user is autherized 
-   res.send("all data is sent");
-})
-app.get("/admin/deleteuser",(req,res)=>{
-    //logic of cheking if the user is autherized
-   res.send("user is deleted");
-})
+
+
  //listening on a port
  app.listen(3000 ,()=>{
     console.log("listening on port");
