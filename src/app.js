86805f1c95e58b47881486e3edbe678ec5773735
Nandini675 <nandinisharma.5698@gmail.com>
2025-03-27 20:41:36 +0530

    making dynamic signup api

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 const express = require ("express");
 //craeting an instance off express js application
- const app= express();
- app.get("/getuserdata",(req,res)=>{
-//   logic of db call and get user data
-try{
-throw new Error("nononokn");
-   res.send("user data is sent");
-}
-catch(err){
-   res.status(404).send("user data is not");
-}
- })
- app.use("/",(err,req,res,next)=>{
-   if(err){
-      res.status(500).send("smthg is wrong");
-   }
- })
+require("dotenv").config();
+const connectDB = require("./config/database");
 
- 
+ const app= express();
+ const User = require("./models/user");
+app.use(express.json());
+ app.post("/signup", async (req,res) => {
+
+  console.log(req.body);
+  //creating a nwe instance of the user model
+  const user = new User(req.body);
+  try{
+   await user.save();
+   res.send("User registered successfully");
+  }
+  catch(err){
+    res.status(400).send("error in registering user");
+  }
+})
+
+ connectDB().then(()=>{
+   console.log("database connection established");
+   app.listen(3000 ,()=>{
+      console.log("listening on port");
+   });
+
+}).catch(err =>{
+  console.error("database connection error");
+})
 
 
  //listening on a port
- app.listen(3000 ,()=>{
-    console.log("listening on port");
- });
