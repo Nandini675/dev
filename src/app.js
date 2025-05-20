3057d37ac5debae6ec263f28b53907e10c303600
Nandini675 <nandinisharma.5698@gmail.com>
2025-05-21 02:22:45 +0530

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -16,11 +16,83 @@ app.use(express.json());
    res.send("User registered successfully");
   }
   catch(err){
-    res.status(400).send("error in registering user");
+    res.status(400).send(err.message);
   }
 })
 
- connectDB().then(()=>{
+//get user by email
+app.get("/user", async (req,res)=>{
+  const useremail= req.body.emailId;
+    try 
+    {
+      
+      const user= await User.find({emailId:useremail});
+     if(user.length==0){
+      res.status(404).send("user not found");
+     }
+     else{
+      res.send(user);}}
+    catch(err){
+      res.status(400).send("error in getting user");
+    }
+})
+//Feed api-get/feed: get all the user from the db
+
+
+app.get("/feed", async (req,res)=>{
+
+  try{
+    const user=  await User.find({});
+    res.send(user);
+  }
+  catch(err){
+    res.status(400).send("error in getting user");
+  }
+})
+// dleet a ser from the db
+ app.delete("/user",async(req,res)=>{
+const userid= await req.body.userId;
+try{
+  const user= await User.findByIdAndDelete({ _id:userid},);
+  if(!user){
+    res.status(404).send("User not found");
+  }
+  else{
+    res.send("userdelete success");
+  }
+}
+catch(err){
+  res.status(500).send("server error");
+
+ }})
+// update  the data of user
+app.patch("/user/:userId",async(req,res)=>{
+  const userid= req.params?.userId;
+  const data = req.body;
+ 
+  
+  try{
+    const Allowed_UPDATES=["age","about","location","photoUrl","experienceLevel","lookingFor", "skills"]
+  const isupdateallowed= Object.keys(data).every((k)=>
+    Allowed_UPDATES.includes(k)
+);
+ if(!isupdateallowed)
+ {throw new Error("update not allowed")}
+  if(data?.skills.length>10){
+    throw new Error("skills can't be more than 10");
+  }
+     const user =await User.findByIdAndUpdate({_id:userid},data,{
+      returnDocument:"after",
+      runValidators:true,
+    });
+    console.log(user);
+    res.send( "user updated succesfully");
+  }
+  catch(err){
+    res.status(500).send("UPDATE FAILED"+err.message);
+  }
+}),
+connectDB().then(()=>{
    console.log("database connection established");
    app.listen(3000 ,()=>{
       console.log("listening on port");
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,99 @@
 const mongoose = require("mongoose");
 const userSchema =  new mongoose.Schema({
   firstName:{
-    type: String
+    type: String,
+    required:true,
+    trim: true,
+    minlength: 2,
+   maxlength: 50,
+   match: /^[A-Za-z]+$/   // Only alphabets
   },  
   lastName:{
-    type:  String
+    type:  String,
+    required:true,
+    trim: true,
+minlength: 2,
+maxlength: 50,
+match: /^[A-Za-z]+$/   // Only alphabets
   },
   emailId:{
-    type:String
+    type:String,
+    lowercase:true,
+    required:true,
+    unique: true,
+    trim:true,
+    match: /^\S+@\S+\.\S+$/  // Simple email regex
+
   },
    password:{
-    type:String
+    type:String,
+    required:true,
+    minilength:8,
+    validate: {
+  validator: function (value) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+  },
+  message: "Password must include uppercase, lowercase, number, and special character"
+}
   },
   age:{
-    type: Number
+    type: Number,
+    min:18
   },
   gender:{
-    type: String
-  }
-})
+    type: String,
+    validate(value){
+       if(!["male","female","others"].includes(value)){
+        throw new Error("Invalid gender");
+       }
+    }
+  }, 
+  photoUrl:{
+    type:String,
+    default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvts5aHBstDkR8PigS4RmZkbZy78zpZoSuOw&s",
+    match: /^https?:\/\/.+/ // Ensure it’s a valid URL
+  },
+  about:{
+    type:String,
+    maxlength: 300,
+    default:" this the default about the user"
+  },
+location: {
+  type: String,
+  trim: true,
+  maxlength: 100,
+  default: "Remote"
+},
+
+experienceLevel: {
+  type: String,
+  enum: ["beginner", "intermediate", "expert"],
+ 
+},
+
+lookingFor: {
+  type: [String],
+  validate: {
+    validator: function (arr) {
+      const validOptions = ["co-founder", "mentor", "teammate", "investor", "collaborator"];
+      return arr.every(val => validOptions.includes(val));
+    },
+    message: "Invalid 'lookingFor' value(s)"
+  },
+ 
+},
+
+  skills:{
+    type:[String],
+    validate:{
+      validator:function(arr){
+        return arr.length<=10;
+      },
+      message:"you can add a maximum of 10 skills "
+    }
+  },
+
+  
+},{timestamps:true})
   const userModel = mongoose.model("User", userSchema);
  module.exports = userModel;
