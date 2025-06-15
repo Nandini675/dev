70e79060f2316d0c9976d16be1e102b60e8b34a5
Nandini675 <nandinisharma.5698@gmail.com>
2025-06-15 21:18:03 +0530

    changing origin

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const { Error } = require("mongoose");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devconnect-frontened.vercel.app/",
     credentials: true,
   })
 );
